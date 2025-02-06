5265566c93d6c987133ac26973d038100723e420
EnderSlain456 <74849071+EnderSlain456@users.noreply.github.com>
2025-02-06 09:50:11 +0000

    Update kernalCrash.cpp
    Will Mess with this in the main crash logic, this theorectically causes an instant BSOD

--- kernalCrash.cpp
+++ kernalCrash.cpp
@@ -1,1 +1,15 @@
+#include <ntddk.h>
 
+extern "C" {
+  void DriverUnload(PDRIVER_OBJECT DriverObject) {
+    UNREFERENCED_PARAMETER(DriverObject);
+  }
+  NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+    UNREFERENCED_PARAMETER(RegistryPath);
+    DriverObject->DriverUnload = DriverUnload;
+
+    KeBugCheckEx(0xDEADDEAD, 0, 0, 0 ,0);
+
+    return STATUS_SUCCESS;
+  }
+}
